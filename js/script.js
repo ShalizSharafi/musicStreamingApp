2fcc15b273b8c8cc4f72a32e2f495ad68a9a39d7
shalizsharafi <shalizoputh@gmail.com>
2026-08-24 00:27:24 -0700

    sidebar ui done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 ///selected elements
 
 const headerTop = document.querySelector('.topHeader')
-
-
-
+const ham = document.querySelector('.ham')
+const side = document.querySelector('.side')
+const sec = document.querySelectorAll('.sec')
+const heroSection = document.querySelector('.heroSection')
 
 /////topHeader bg change while scrolling
 addEventListener('scroll',()=>{
        let headerHeight = headerTop.clientHeight
        headerTop.classList.toggle('header-bg', headerHeight < scrollY)
 })
+///hamburger menu
+ham.addEventListener('click',()=>{
+       side.classList.toggle('sideClose')
+       sec.forEach((val)=>{
+              val.classList.toggle('secWide')
+       })
+})
+
